ebac6dba9615341d9bd49362e2d1e6e1d022f0e7
Linguini <leobrazfonseca24@gmail.com>
2022-08-07 02:13:00 -0300

    iniciando trabalhos

--- music.py
+++ music.py
@@ -9,11 +9,11 @@ class MyClient(discord.Client):
     async def on_message(self, message):
         print('Message from {0.author}: {0.content}'.format(message))
 
-bot = commands.Bot(command_prefix='!')
+bot = commands.Bot(command_prefix='.')
 
 @bot.command()
-async def test(ctx, arg):
-    await ctx.send(arg)
+async def info(ctx):
+    await ctx.send(ctx.author)
 
 client = MyClient()
 client.run('MTAwNTYxMjE4NDkyOTMyMDk5MA.GzIJK3.y9ZewjyeyS2l6Rlvne4UhwKBacYsmUfW8U9efg')
