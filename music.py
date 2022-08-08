4834971abeb2eab04f74e29237cb1feb4a016411
Linguini <leobrazfonseca24@gmail.com>
2022-08-07 23:18:12 -0300

    bot v1 sem musica

--- music.py
+++ music.py
@@ -1,19 +1,27 @@
+from ast import alias
+from unicodedata import name
 import discord
 from discord.ext import commands
 import os
-
-class MyClient(discord.Client):
-    async def on_ready(self):
-        print('Logged on as {0}!'.format(self.user))
-
-    async def on_message(self, message):
-        print('Message from {0.author}: {0.content}'.format(message))
+from youtube_dl import YoutubeDL
+#from ytf import music_cog
 
 bot = commands.Bot(command_prefix='.')
 
-@bot.command()
-async def info(ctx):
+@bot.command(name="id", help="comando para exibir seu id no discord")
+async def id(ctx):
     await ctx.send(ctx.author)
 
-client = MyClient()
-client.run('MTAwNTYxMjE4NDkyOTMyMDk5MA.GzIJK3.y9ZewjyeyS2l6Rlvne4UhwKBacYsmUfW8U9efg')
+@bot.command(name="udsu", aliases=["u"], help="comando para nosso imortal")
+async def udsu(ctx):
+    await ctx.send("Udson Bolas, nosso IMORTAL!")
+
+@bot.command()
+async def adm(ctx):
+    await ctx.send("O SUPRASSUMO DA ORDEM, BRUNELO!")
+
+@bot.command()
+async def thati(ctx):
+    await ctx.send("Vc procura pela Thatiana A TROLL?")
+
+bot.run('MTAwNTYxMjE4NDkyOTMyMDk5MA.GzIJK3.y9ZewjyeyS2l6Rlvne4UhwKBacYsmUfW8U9efg')
