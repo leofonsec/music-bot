af2faa2bd6bb07023205fc709e852fdbd9ec5a35
Linguini <leobrazfonseca24@gmail.com>
2022-08-08 23:32:52 -0300

    bot v1 com função p/ canal

--- music.py
+++ music.py
@@ -12,23 +12,31 @@ import ffmpeg
 class music_cog(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+        self.is_playing = False
     
-    @commands.command(aliases = ['j'], help = "Comando pro Vagnao conectar.")
-    async def join(self, ctx):
-        if ctx.author.voice is None:
-            await ctx.send("Entra num canal ae chupinga")
+    async def teste(self, ctx):
         vchannel = ctx.author.voice.channel
         if ctx.voice_client is None:
             await vchannel.connect()
         else:
             await ctx.voice_client.move_to(vchannel)
+
     
-    @commands.command(aliases = ['d', 'leave'], help = "Comando pro Vagnao desconectar.")
+    @commands.command(aliases = ['j'], help = "Comando pro Vagnao conectar.")
+    async def join(self, ctx):
+        if ctx.author.voice is None:
+            await ctx.send("Entra num canal ae chupinga")
+        if self.is_playing == False:
+            await self.teste(ctx)
+    
+    @commands.command(aliases = ['d', 'leave', 'l'], help = "Comando pro Vagnao desconectar.")
     async def disconnect(self, ctx):
         await ctx.voice_client.disconnect()
 
     @commands.command(aliases = ['p'], help = "Comando pro Vagnao tocar seu som!")
     async def play(self, ctx, url):
+        if self.is_playing == False:
+            await self.teste(ctx)
         ctx.voice_client.stop()
         FFMPEG_OPTIONS = {'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5', 'options': '-vn'}
         YDL_OPTIONS = {'format':"bestaudio"}
@@ -43,9 +51,11 @@ class music_cog(commands.Cog):
     @commands.command()
     async def pause(self, ctx):
         await ctx.voice_client.pause()
+        await ctx.voice_client.is_paused()
         await ctx.send("Pausudo... Digo, pausado")
     
     @commands.command()
     async def resume(self, ctx):
         await ctx.voice_client.resume()
+        await ctx.voice_client.is_playing()
         await ctx.send("Voltando")
