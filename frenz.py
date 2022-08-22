abb7a95e28201f1181c9f4945a41e4109ddb719c
Linguini <leobrazfonseca24@gmail.com>
2022-08-22 19:42:57 -0300

    bot v1

--- frenz.py
+++ frenz.py
@@ -4,18 +4,18 @@ from discord.ext import commands
 import os
 
 class frenz_cog(commands.Cog):
-    @commands.command(name="id", help="comando para exibir seu id no discord")
+    @commands.command(name="id", help="Comando para exibir seu id no discord")
     async def id(self, ctx):
         await ctx.send(ctx.author)
 
-    @commands.command(name="udsu", aliases=["u"], help="comando para nosso imortal")
+    @commands.command(name="udsu", aliases=["u"], help="Comando para nosso imortal")
     async def udsu(self, ctx):
         await ctx.send("Udson Bolas, nosso IMORTAL!")
 
-    @commands.command()
+    @commands.command(help="Comando para nosso saudoso ADM")
     async def adm(self, ctx):
         await ctx.send("O SUPRASSUMO DA ORDEM, BRUNELO!")
 
-    @commands.command()
+    @commands.command(help="Comando para a mais troll do mundo")
     async def thati(self, ctx):
         await ctx.send("Vc procura pela Thatiana A TROLL?")
--- music.py
+++ music.py
@@ -13,8 +13,10 @@ class music_cog(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         self.is_playing = False
+        self.queue = []
+        self.vc = None
     
-    async def teste(self, ctx):
+    async def joincom(self, ctx): #command for connect to a voice channel
         vchannel = ctx.author.voice.channel
         if ctx.voice_client is None:
             await vchannel.connect()
@@ -27,7 +29,7 @@ class music_cog(commands.Cog):
         if ctx.author.voice is None:
             await ctx.send("Entra num canal ae chupinga")
         if self.is_playing == False:
-            await self.teste(ctx)
+            await self.joincom(ctx)
     
     @commands.command(aliases = ['d', 'leave', 'l'], help = "Comando pro Vagnao desconectar.")
     async def disconnect(self, ctx):
@@ -36,26 +38,29 @@ class music_cog(commands.Cog):
     @commands.command(aliases = ['p'], help = "Comando pro Vagnao tocar seu som!")
     async def play(self, ctx, url):
         if self.is_playing == False:
-            await self.teste(ctx)
+            await self.joincom(ctx)
+
         ctx.voice_client.stop()
         FFMPEG_OPTIONS = {'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5', 'options': '-vn'}
         YDL_OPTIONS = {'format':"bestaudio"}
         song = ctx.voice_client
-
+        
         with youtube_dl.YoutubeDL(YDL_OPTIONS) as ydl:
             info = ydl.extract_info(url, download=False)
             url2 = info ['formats'][0]['url']
             source = await discord.FFmpegOpusAudio.from_probe(url2, **FFMPEG_OPTIONS)
             song.play(source)
-    
+
+        self.queue.append(song)
+        await ctx.send("add à fila")        
+         
     @commands.command()
     async def pause(self, ctx):
-        await ctx.voice_client.pause()
-        await ctx.voice_client.is_paused()
         await ctx.send("Pausudo... Digo, pausado")
+        await ctx.voice_client.pause() 
     
     @commands.command()
     async def resume(self, ctx):
-        await ctx.voice_client.resume()
-        await ctx.voice_client.is_playing()
         await ctx.send("Voltando")
+        await ctx.voice_client.resume()
+        
